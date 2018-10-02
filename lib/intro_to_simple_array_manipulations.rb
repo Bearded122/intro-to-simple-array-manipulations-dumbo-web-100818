8536b49147f10257c9e5e7b78b1f02df22a9cc97
John Sime <johnc.sime@gmail.com>
2018-10-02 08:18:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,39 @@ end
 def using_pop(array)
   array.pop
 end
+
+def pop_with_args(array, string)
+  array.pop(2)
+end
+
+def using_unshift(array, string)
+  array.unshift
+end
+
+def shift_with_args(array, string)
+  array.unshift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, item)
+  array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
